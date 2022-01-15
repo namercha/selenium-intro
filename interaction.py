8472cc678d2312cb311d2c8b0902c9df93dc56ff
nabil <merchant.nabil@gmail.com>
2022-01-15 12:10:53 -0600

    Searches wikipedia for "Python" articles.

--- interaction.py
+++ interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # Gets the number of articles in English from the wikipedia home page
 chrome_driver_path = "/Users/nabil/Documents/code/chromedriver"
@@ -9,3 +10,14 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
 print(article_count.text)
+
+# Click on a link
+article_count.click()
+
+# all_portals = driver.find_element(By.LINK_TEXT, "All Portals")
+# all_portals.click()
+
+# Typing in a search bar. This searches for "Python" in the search bar in wikipedia front page.
+search_bar = driver.find_element(By.NAME, "search")
+search_bar.send_keys("Python")
+search_bar.send_keys(Keys.ENTER)
